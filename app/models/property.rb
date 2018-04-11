3727ef47a85d857af2ccd14ae2b1ccc3072f6434
Ray Suyoto <ray.suy@icloud.com>
2018-04-11 10:48:54 -0600

    backend

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,12 @@
 class Property < ApplicationRecord
   belongs_to :agent
   has_one :address
+
+  def self.available
+    select('properties.id, price, beds, baths, sq_ft, ad.city, ad.street, a.first_name, a.last_name, a.email, a.id AS agent_id')
+    .joins('INNER JOIN agents a ON a.id = properties.agent_id INNER JOIN addresses ad ON ad.property_id = properties.id')
+    .where('properties.sold <> TRUE')
+    .order('a.id')
+  end
+
 end
